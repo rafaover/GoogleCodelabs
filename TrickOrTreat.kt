afc4b7554fa94ad7223a57e409321402e478e6d5
[rafamsilva] <[rafamsilva@gmail.com]>
2022-08-24 13:16:07 +1000

    Added TrickOrTreat Function and trick/treat Lambdas

--- TrickOrTreat.kt
+++ TrickOrTreat.kt
@@ -14,3 +14,23 @@ fun main() {
     trickFunction()
     treatFunction()
 }
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: (Int) -> String): () -> Unit {
+    return if (isTrick) {
+        trick
+    }
+    else {
+        println(extraTreat(5))
+        treat
+    }
+}
+
+//lambda expression -- { } inside a variable, which turns in a function.
+//"trick" is a function in this lambda expression.
+val trick = {
+    println("No treats!")
+}
+//Specifying a data type. "Unit" in this case is the RETURN TYPE of the function.
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
