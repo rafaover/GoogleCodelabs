3cd6fb738ab1a616f79d61885906ae697874243b
rafaover <rafamsilva@gmail.com>
2022-08-24 13:10:21 +1000

    Added SmartLightDevice and SmartHome Classes

--- SmartHomeDevice.kt
+++ SmartHomeDevice.kt
@@ -53,3 +53,80 @@ class SmartTvDevice(deviceName: String, deviceCategory: String):
         println("Channel number decreased to $channelNumber.")
     }
 }
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart Light"
+    private var brightnessLevel by RangeRegulator(initialValue = 2, minValue = 0, maxValue = 100)
+
+    override fun turnOn() {
+        super.turnOn()
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+    override fun turnOff() {
+        super.turnOff()
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
+    fun increaseBrightness() {
+        brightnessLevel++
+    }
+    fun decreaseBrightness() {
+        brightnessLevel--
+    }
+}
+
+class SmartHome (
+    private val smartTvDevice: SmartTvDevice,
+    private val smartLightDevice: SmartLightDevice
+) {
+    var deviceTurnOnCount = 0
+        private set
+
+    //method calling methods
+    fun turnOnTv() {
+        deviceTurnOnCount++
+        smartTvDevice.turnOn()
+    }
+    private fun turnOffTv() {
+        deviceTurnOnCount--
+        smartTvDevice.turnOff()
+    }
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+    fun decreaseTvVolume() {
+        smartTvDevice.decreaseVolume()
+    }
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+    fun changeTvChannelToPrevious() {
+        smartTvDevice.previousChannel()
+    }
+    fun turnOnLight() {
+        deviceTurnOnCount++
+        smartLightDevice.turnOn()
+    }
+    private fun turnOffLight() {
+        deviceTurnOnCount--
+        smartLightDevice.turnOff()
+    }
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+    fun decreaseLightBrightness() {
+        smartLightDevice.decreaseBrightness()
+    }
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
+    }
+    fun printSmartTvInfo() {
+        smartTvDevice.printDeviceInfo()
+    }
+    fun printSmartLightInfo() {
+        smartLightDevice.printDeviceInfo()
+    }
+}
