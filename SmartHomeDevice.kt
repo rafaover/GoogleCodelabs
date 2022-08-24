3368df372155fabcb231c211a1aaeb2801a62e68
rafaover <rafamsilva@gmail.com>
2022-08-24 13:09:00 +1000

    Added SmartTVDevice Class

--- SmartHomeDevice.kt
+++ SmartHomeDevice.kt
@@ -15,3 +15,41 @@ open class SmartDevice (val name: String, val category: String) {
         println("Device Name: $name, Category: $category, Type: $deviceType")
     }
 }
+
+class SmartTvDevice(deviceName: String, deviceCategory: String):
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart TV"
+    private var speakerVolume by RangeRegulator(initialValue = 0, minValue = 0, maxValue = 100)
+    private var channelNumber by RangeRegulator(initialValue = 1, minValue = 0, maxValue = 200)
+
+    override fun turnOn() {
+        super.turnOn()
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                    "set to $channelNumber."
+        )
+    }
+    override fun turnOff() {
+        super.turnOff()
+        println("$name turned off")
+    }
+    //method
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+    fun decreaseVolume() {
+        speakerVolume--
+        println("Speaker volume decreased to $speakerVolume.")
+    }
+    //method
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+    fun previousChannel() {
+        channelNumber--
+        println("Channel number decreased to $channelNumber.")
+    }
+}
