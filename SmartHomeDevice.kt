5a6e40ee592c94cdb63068cf71429712e96e045b
rafaover <rafamsilva@gmail.com>
2022-09-05 11:37:03 +1000

    4o Exercise - Song Catalog (2o Option)

--- SmartHomeDevice.kt
+++ SmartHomeDevice.kt
@@ -4,13 +4,8 @@ import kotlin.reflect.KProperty
 open class SmartDevice (val name: String, val category: String) {
     private var deviceStatus = "online"
     open val deviceType = "unknown"
-
-    open fun turnOn(){
-        deviceStatus = "on"
-    }
-    open fun turnOff(){
-        deviceStatus = "off"
-    }
+    open fun turnOn(){deviceStatus = "on"}
+    open fun turnOff(){deviceStatus = "off"}
     fun printDeviceInfo() {
         println("Device Name: $name, Category: $category, Type: $deviceType")
     }
--- out/production/GoogleLearning/README.md
+++ out/production/GoogleLearning/README.md
@@ -3,4 +3,4 @@ Google Learning Projects.
 
 1. SmartHome - A little code to learn classes. Main goal is to activate some home appliances and light switches at home.
 2. Trick Or Treat
-3. 
+3. Exercises
