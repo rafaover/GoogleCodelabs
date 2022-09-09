c7bf1fcac4ca350cd8588529b5b2a38ff7a433c9
rafaover <rafamsilva@gmail.com>
2022-09-09 19:30:22 +1000

    Added SpecialAuction.kt Updated Gitignore

--- SpecialAuction.kt
+++ SpecialAuction.kt
@@ -5,12 +5,10 @@ fun main() {
     println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
     println("Item B is sold at ${auctionPrice(null, 3000)}.")
 }
-
 class Bid(val amount: Int, val bidder: String)
 
 fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
-    // Fill in the code.
-
+    return bid?.amount ?: minimumPrice
 }
 // OUTPUT
 // Item A is sold at 5000.
--- out/production/GoogleLearning/README.md
+++ out/production/GoogleLearning/README.md
@@ -3,4 +3,10 @@ Google Learning Projects.
 
 1. SmartHome - A little code to learn classes. Main goal is to activate some home appliances and light switches at home.
 2. Trick Or Treat
-3. Exercises
+3. Movie Ticket Price
+4. Temperature Converter
+5. Phone Notification
+6. Internet Profile
+7. Foldable Phone
+8. Song Catalog
+9. Special Auction
