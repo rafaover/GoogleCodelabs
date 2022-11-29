77a66ea9eb5de87e7512f28b2cdd53d00e1561d6
Rafael Moreira <rafamsilva@gmail.com>
2022-11-29 13:08:05 +1100

    Update building.kt
    Added fun getroom() and fun carpetmeasure()

--- building.kt
+++ building.kt
@@ -1,4 +1,11 @@
-import kotlin.math.PI
+/**
+* Program that implements classes in different types of constructions
+* Shows how to:
+* Create class hierarchy, variables and functions with inheritance,
+* abstract class, overriding, and private vs. public variables.
+*/
+
+import kotlin.math.*
 
 fun main(){
     val squareCabin = SquareCabin(6, 50.0)
@@ -26,19 +33,33 @@ fun main(){
     println("Material: ${buildingMaterial}")
     println("Capacity: ${capacity}")
     println("Has room? ${hasRoom()}")
+    getRoom()
+    println("Has room? ${hasRoom()}")
+    getRoom()
     println("Floor area: %.2f".format(floorArea()))
+    println("Carpet Length: ${calculateMaxCarpetLength()}")
 	}
     
     with(roundTower) {
     println("\nRound Tower\n==========")
     println("Material: ${buildingMaterial}")
     println("Capacity: ${capacity}")
-    println("Has room? ${hasRoom()}")
     println("Floor area: ${floorArea()}")
     println("Floor area: %.2f".format(floorArea()))
+    println("Carpet Length: ${calculateMaxCarpetLength()}")
 	}
    
 }
+
+/**
+* Defines properties common to all dwellings.
+* All dwellings have floorspace,
+* but its calculation is specific to the subclass.
+* Checking and getting a room are implemented here
+* because they are the same for all Dwelling subclasses.
+*
+* @param residents --> Current number of residents
+*/
 
 // It's named as abstract because doesn't pass/have any values
 abstract class Dwelling(private var residents: Int) {
@@ -50,7 +71,21 @@ abstract class Dwelling(private var residents: Int) {
         return residents < capacity
     }
     
-    abstract fun floorArea(): Double 
+    abstract fun floorArea(): Double
+    
+    /**
+    * Compares the capacity to the number of residents and
+    * if capacity is larger than number of residents,
+    * add resident by increasing the number of residents.
+    */
+    fun getroom() {
+        if (hasRoom()) {
+            residents++
+            println("You got a room!")
+        } else {
+        	println("Sorry, at capacity and no rooms left.")
+        }
+    }
 }
     
 class SquareCabin(residents: Int, val length: Double) : Dwelling(residents) {
@@ -70,6 +105,11 @@ open class RoundHut(residents: Int, val radius: Double) : Dwelling(residents){
     override fun floorArea(): Double { 
         return PI * radius * radius 
     }
+    
+    fun calculateMaxCarpetLength(): Double {
+    	return sqrt(2.0) * radius
+	}
+    
 }
 
 class RoundTower(residents: Int, radius: Double, 
