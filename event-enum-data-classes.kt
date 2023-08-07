7190f30b3d2d5f717d24f7b977d7445dea443fdc
Rafael Moreira <rafamsilva@gmail.com>
2023-08-07 15:49:35 +1000

    Update event-enum-data-classes.kt
    Method to count short events

--- event-enum-data-classes.kt
+++ event-enum-data-classes.kt
@@ -10,14 +10,22 @@ data class Event (
 )
 
 var events = mutableListOf<Event>(
-	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
-	Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15),
+    Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
+   	Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15),
 	Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30),
 	Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60),
-	Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
+    Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
 	Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45),
 )
 
 fun main() {
-    println(events)
+    var shortEventsCount = 0
+    events.forEach {
+    	if (it.durationInMinutes < 60) shortEventsCount++
+    }
+    // OR
+    // val shortEvents = events.filter { it.durationInMinutes < 60 }
+    // println("You have ${shortEvents.size} short events.")
+    
+    println("You have ${shortEventsCount} short events.")
 }
