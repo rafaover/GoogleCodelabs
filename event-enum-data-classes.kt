83fe297f0b439d0bee49978641e9fe1946a97719
Rafael Moreira <rafamsilva@gmail.com>
2023-08-07 15:42:13 +1000

    Update event-enum-data-classes.kt
    updated mutableList with events
    changed minutes variable name

--- event-enum-data-classes.kt
+++ event-enum-data-classes.kt
@@ -6,19 +6,18 @@ data class Event (
     val title: String,
     val description: String? = null,
     val daypart: Daypart,
-    val duration: Int
+    val durationInMinutes: Int
 )
 
-var events = mutableListOf<Event>()
+var events = mutableListOf<Event>(
+	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
+	Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15),
+	Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30),
+	Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60),
+	Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
+	Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45),
+)
 
 fun main() {
-	events.add(
-        Event(
-            "Study Kotlin",
-            "Commit to studying Kotlin at least 15 minutes per day.",
-            Daypart.EVENING,
-            15
-        )
-    )
     println(events)
 }
